8acd4304b0cde448ca145d9b7d041a55fe240cef
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2023-05-14 19:29:06 +0530

    Create Exercise2.java

---
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -0,0 +1,21 @@
+package io.javabrains.reactiveworkshop;
+
+import java.io.IOException;
+
+public class Exercise2 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
+
+        // Print all numbers in the ReactiveSources.intNumbersFlux stream
+        ReactiveSources.intNumbersFlux().subscribe(e -> System.out.println(e));
+
+        // Print all users in the ReactiveSources.userFlux stream
+        ReactiveSources.userFlux().subscribe(user -> System.out.println(user));
+
+        System.out.println("Press a key to end");
+        System.in.read();
+    }
+
+}
